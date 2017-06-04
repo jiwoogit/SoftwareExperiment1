c0fee3edacd72e3dfd8dcbbdb6e6265f7700a847
Jung Dongwon <dg52316@naver.com>
2017-06-04 14:53:08 +0900

    //first draft ended

--- SoftwareExperimentProject/src/Customer.java
+++ SoftwareExperimentProject/src/Customer.java
@@ -1,11 +1,6 @@
 
 
 public class Customer{
-	public MenuItem order(String menuName, Menu menu, Bartender bartender){
-		MenuItem menuItem = menu.choose(menuName);
-		return menuItem;
-		
-	}
-
+	
 }
 
